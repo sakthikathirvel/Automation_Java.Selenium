5c9218544487f56069e3507dcc5ba4ade2dbbcf6
sakthikathirvel <sakthivelkathirvel1013@gmail.com>
2026-03-05 14:12:55 +0530

    Add files via upload
    week 2

---
+++ week2/demo1.java
@@ -0,0 +1,11 @@
+package week2;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo1 {
+    public static void main(String[] args) {
+ChromeDriver driver=new ChromeDriver();
+driver.get("http://leaftaps.com/opentaps/control/main");
+}
+
+}
