2e4d0c0a2c2837b8c63d99f36d66f6eb4ab3bfbe
sakthivel <sakthivelkathirvel1013@gmail.com>
2026-03-05 15:10:28 +0530

    Add files via upload

---
+++ week2/demo2.java
@@ -0,0 +1,19 @@
+package week2.day1;
+import java.util.Scanner;
+
+public class demo2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        double score = scan.nextDouble();
+        scan.nextLine();
+        String dept = scan.nextLine();
+        System.out.println(name);
+        System.out.println(score/10);
+        System.out.println(dept);
+
+        
+
+
+}
+}
