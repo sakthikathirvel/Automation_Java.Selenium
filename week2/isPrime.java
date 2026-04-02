80e56b8df0f433eb41fc8f6a0f596a0d75185609
sakthivel <sakthivelkathirvel1013@gmail.com>
2026-04-02 21:07:32 +0530

    IsPrimeNumber-Assignment

---
+++ week2/isPrime.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class isPrime {
+
+    public static void main(String[] args) {
+        int num=13;
+        boolean isPrime=true;
+         for(int i=2;i<13;i++){
+            if(num%i==0){
+
+                isPrime=false;
+                break;
+
+            }else{             
+            }           
+        }
+            System.out.println(isPrime);
+         }
+
+
+
+
+        
+    }
+
